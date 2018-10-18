0d2d6614ae2b2e4b33d8665d165668a0d8168f8f
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-10-18 19:10:46 +0300

    [create react app with webpack]

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container ">
+            <div align="center" className="mt-2">
+            <h1>Hello World</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
